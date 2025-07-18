636899b5eaf4546a24065b7f1339abf544343174
larisa.koryukova <lara.lara@mail.ru>
2025-07-18 16:09:06 +0400

    minor changes in syntax

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,19 @@
 from datetime import datetime
 
-def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
-    """
-    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-    state соответствует указанному значению
 
-    """
+def filter_by_state(list_of_dict: list, state="EXECUTED") -> list:
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению"""
     filtered_list = []
     for dict_item in list_of_dict:
-        if dict_item.get('state') == state:
+        if dict_item.get("state") == state:
             filtered_list.append(dict_item)
 
     return filtered_list
 
-def sort_by_date(data_list: list, data_key='date', descending=True) -> list:
-    """
-    Функция, которая принимает список словарей и необязательный параметр,
+
+def sort_by_date(data_list: list, data_key="date", descending=True) -> list:
+    """Функция, которая принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать
-     новый список, отсортированный по дате (date).
-     """
-    return sorted(data_list, key=lambda x: datetime.strptime(x[data_key], '%Y-%m-%dT%H:%M:%S.%f'), reverse=descending)
+     новый список, отсортированный по дате (date)."""
+    return sorted(data_list, key=lambda x: datetime.strptime(x[data_key], "%Y-%m-%dT%H:%M:%S.%f"), reverse=descending)
